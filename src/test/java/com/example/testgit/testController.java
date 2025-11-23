05c4fe0af22f454139b9953b112bd95737f890d3
UnexcellentCoder <1171186090@qq.com>
2025-11-23 21:29:39 +0800

    test02 add grade

--- src/test/java/com/example/testgit/testController.java
+++ src/test/java/com/example/testgit/testController.java
@@ -2,4 +2,12 @@ package com.example.testgit;
 
 public class testController {
     public static final String WORD_TEST = "wordTest";
+
+    private String getName(){
+        return "name";
+    }
+
+    private String getGrade(){
+        return "8年级";
+    }
 }
